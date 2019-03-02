db0b31b7161915ba490733d68dff2976fb30345f
stevenaw <sweerdenburg@gmail.com>
2019-03-02 14:41:48 -0500

    Recursion + Fibonacci

---
+++ 05-recursion-fibonacci/build.bat
@@ -0,0 +1,13 @@
+SET appFile=app.il
+SET libFile=lib.il
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\ilasm.exe "%~dp0%libFile%" /dll /output:stevenaw.dll
+if errorlevel 1 goto reportError
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\ilasm.exe "%~dp0%appFile%" /exe
+if errorlevel 1 goto reportError
+
+exit
+
+:reportError
+  pause
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ A series of small programs to experiment with and learn about CIL.
 1. [Control Structures](./02-control-structures)
 1. [Methods and Functions](./03-method-invocation)
 1. [Types and External Libraries](./04-types-and-externs)
+1. [Recursion and Fibonacci](./05-recursion-fibonacci)
